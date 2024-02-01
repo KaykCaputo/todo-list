f1ca7fa7848b52a0b5152483c101f1323e9e9233
Kayk Caputo <kayk.caputo@gmail.com>
2024-01-31 21:08:39 -0300

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Box } from '@mui/material';
 
 function App() {
   return (
-    <Grid>
+    <Grid container={true}>
       <Grid item xs={12}>
-        <Typography>
-          |To-Do List
+        <Box sx={{padding: '16px'}}>
+        <Typography variant='h4' align='center'>
+          To-Do List
         </Typography>
+        </Box>
       </Grid> 
-      <Grid item xs={12}>
-        <div>Todo List Container</div>
+      <Grid item xs={1} md={2} lg={3}></Grid>
+      <Grid item xs={10} md={8} lg={6}>
+        <div style={{border:"1px solid black"}}>Todo List Container</div>
       </Grid>
+      <Grid item xs={1} md={2} lg={3}></Grid>
     </Grid>
   );
 }
